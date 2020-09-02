289a0e2e660b9288f1ba6114fb019194b5c64f7c
Chris McAulay <cmcaulay07@gmail.com>
2020-09-02 16:05:56 -0600

    limit pages using decorators

--- views/alerts.py
+++ views/alerts.py
@@ -2,17 +2,20 @@ from flask import Blueprint, render_template, request, url_for, redirect, sessio
 from models.alert import Alert
 from models.store import Store
 from models.item import Item
+from models.user import requires_login
 
 alert_blueprint = Blueprint('alerts', __name__)
 
 
 @alert_blueprint.route('/')
+@requires_login
 def index():
     alerts = Alert.find_many_by('user_email', session['email'])
     return render_template('alerts/index.html', alerts=alerts)
 
 
 @alert_blueprint.route('/new', methods=['GET', 'POST'])
+@requires_login
 def create_alert():
     if request.method == 'POST':
         alert_name = request.form['name']
@@ -32,6 +35,7 @@ def create_alert():
     return render_template('alerts/new_alert.html')
 
 @alert_blueprint.route('/edit/<string:alert_id>', methods=['GET', 'POST'])
+@requires_login
 def edit_alert(alert_id):
     alert = Alert.get_by_id(alert_id)
 
@@ -45,6 +49,7 @@ def edit_alert(alert_id):
     return render_template('alerts/edit_alert.html', alert=alert)
 
 @alert_blueprint.route('/delete/<string:alert_id>')
+@requires_login
 def delete_alert(alert_id):
     alert = Alert.get_by_id(alert_id)
     if alert.user_email == session['email']:
