608c14151fbff23523fe0baf8a8718345d5753e7
Chris McAulay <cmcaulay07@gmail.com>
2020-09-02 16:40:34 -0600

    landing page

--- app.py
+++ app.py
@@ -7,11 +7,23 @@ from models.alert import Alert
 from models.store import Store
 import random
 import string
+import os
+from flask.templating import render_template
 
 app = Flask(__name__)
-app.secret_key = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=10))
+app.secret_key = ''.join(random.choices(
+    string.ascii_uppercase + string.ascii_lowercase + string.digits, k=10))
+app.config.update(
+    ADMIN=os.environ.get('ADMIN')
+)
 
 Database.initialize()
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 
 app.register_blueprint(alert_blueprint, url_prefix='/alerts')
 app.register_blueprint(store_blueprint, url_prefix='/stores')
