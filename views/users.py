2891345ad9a7ed77eb6f2f5966d3eb71c06655d7
Chris McAulay <cmcaulay07@gmail.com>
2020-09-03 11:47:17 -0600

    ui improvements and logout

--- views/users.py
+++ views/users.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, redirect, request, session, url_for
+from flask import Blueprint, render_template, redirect, request, session, url_for, flash
 from models.user import User, UserErrors
+import re
 
 user_blueprint = Blueprint('users', __name__)
 
@@ -13,10 +14,11 @@ def register_user():
         try:
             User.register_user(email, password)
             session['email'] = email
-            return email
+            return redirect(url_for('alerts.index'))
 
         except UserErrors.UserError as e:
-            return e.message
+            flash('We already have you in the system, log in below!', 'warning')
+            return redirect(url_for('users.login_user'))
 
     return render_template('users/register.html')
 
@@ -29,8 +31,21 @@ def login_user():
         try:
             if User.is_login_valid(email, password):
                 session['email'] = email
-                return email
-        except UserErrors.UserError as e:
-            return e.message
+                pattern = re.compile(r"^.*(?=(@))")
+                match = pattern.search(email)
+                name = match.group(0)
+                flash('Welcome back, {}!'.format(name), 'success')
+                return redirect(url_for('alerts.index'))
+        except UserErrors.IncorrectPasswordError as e:
+            flash(e.message, 'danger')
+        except:
+            flash("Looks like you're not registered yet, do it now!", 'warning')
+            return redirect(url_for('users.register_user'))
 
     return render_template('users/login.html')
+
+@user_blueprint.route('/logout')
+def logout_user():
+    session['email'] = None
+    flash("See you later!", 'success')
+    return redirect(url_for('users.login_user'))
